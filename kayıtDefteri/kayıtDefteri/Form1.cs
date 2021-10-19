f0ac106ae891df5ef241afef8e4f017347cd7f4e
Ferid Akşahin <83434568+FeridAksahin@users.noreply.github.com>
2021-10-19 03:19:11 +0300

    Update Form1.cs

--- kayıtDefteri/kayıtDefteri/Form1.cs
+++ kayıtDefteri/kayıtDefteri/Form1.cs
@@ -29,25 +29,13 @@ namespace kayıtDefteri
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label3.Text = textBox1.Text; //label3',n texti textboxa yazdıgımız text olur. button1'e bastıgımızda
+            label3.Text = textBox1.Text; 
             label4.Text = textBox2.Text;
 
             comboBox2.Items.Add("İstanbul"); 
             comboBox2.Items.Add("Diyarbakır");
-            /*combobox'ımıza bir normal kod yazmadan eleman ekledik 1. combobox'ımıza direk combox tıklayıp |> tuşuna basıp
-             * edit diyerek ekledik elemanları ilçeleri yani. Combobox'ları il.e eklemek için kullandık. comboBox2 mizi ise
-             * kod yazarak eleman ekledik. Fakat uygulama başlatıldığında comboBox2 boştur kaydet butonuna yani button1'e
-             * basınca comboBox2'deki elemanlar gelir çünkü button1_click metodunun içine yazdık yani button1 click
-             * oldugunda yapılmasını istediğimiz metodun içi */
-
-            label8.Text = comboBox1.Text; //ComboBox1'de seçilen Texti label8 e eşitledik
+            label8.Text = comboBox1.Text; 
             label11.Text = textBox3.Text;
-            /*
-             label11'deki texti textBox3'te yazılan texte atadık. Yani textBox3'te ne yazarsak label11  de o yazar adres yani
-            adres textBox'ını uzattık textboxa basıp |> butonuna tıklayıp multiLine özelliğini aktif ettik çünkü bu aktif
-            değil iken textBox enterlanmıyor yazınca yani aşağı geçmiyor tek satır oluyor.
-             */
-
         }
 
         private void label6_Click(object sender, EventArgs e)
